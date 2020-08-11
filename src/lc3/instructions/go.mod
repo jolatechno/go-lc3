69e4e7070fd09703494c7f13f6460b8af9ae1b1a
jolatechno <joseph.touzet@gmail.co-N>
2020-08-11 12:32:49 +0200

    updating lc3 instruction type

--- src/lc3/instructions/go.mod
+++ src/lc3/instructions/go.mod
@@ -1,5 +1,5 @@
 module github.com/jolatechno/go-lc3/src/lc3/instructions
 
-require github.com/jolatechno/go-lc3/src/interfaces v0.0.0-20200811080702-672fcc95b73f
+require github.com/jolatechno/go-lc3/src/interfaces v0.0.0-20200811102957-bbe7ed809550
 
 go 1.13
--- src/lc3/instructions/instructions.go
+++ src/lc3/instructions/instructions.go
@@ -29,14 +29,14 @@ const (
 /* defining a basic interface for lc3 instructions */
 type Lc3Instruction struct {
   OP uint8 /* instruction opcode, uint4 would have been more appropriate but isn't present in standard go */
-  Exec func(memory interfaces.Memory, registers interfaces.Registers, param uint16) (err error) /* function serving intstruction's execution */
+  Exec func(memory interfaces.Memory, registers interfaces.Registers, param uint16) (next bool, err error) /* function serving intstruction's execution */
 }
 
 /* defining the interface */
-func (instru *Lc3Instruction)Run(memory interfaces.Memory, registers interfaces.Registers, params []interface{}) (err error) {
+func (instru *Lc3Instruction)Run(memory interfaces.Memory, registers interfaces.Registers, params []interface{}) (next bool, err error) {
   intParam, ok := params[0].(uint16) /* convert param to int */
   if !ok { /* return an error if not possible */
-    return errors.New("register not understood")
+    return false, errors.New("register not understood")
   }
 
   return instru.Exec(memory, registers, intParam) /* execute the instruction */
