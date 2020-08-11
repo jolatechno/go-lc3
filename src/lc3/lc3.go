a92751feedbb0613a00769b449ec835429e90936
jolatechno <joseph.touzet@gmail.co-N>
2020-08-11 14:26:30 +0200

    added a load function to load the program to memory

--- src/lc3/lc3.go
+++ src/lc3/lc3.go
@@ -7,14 +7,21 @@ import (
   "github.com/jolatechno/go-lc3/src/lc3/instructions"
 )
 
-func Exec(prgm []uint16) (err error){
-  pc_start := 0x3000 /* init the pc to a certain value */
+var (
+  PC_START = 0x3000 /* init the pc to a certain value */
+  Origin = 0x0000 /* origin of program load */
+)
 
+func LoadPrgm(prgm []uint16) (n int, err error) {
   interfacePrgm := make([]interface{}, len(prgm)) /* create an array of interfaces */
   for i, v := range prgm { /* and populate it with the program */
     interfacePrgm[i] = v
   }
 
-  memory.Lc3mem.Writea(pc_start, interfacePrgm) /* write the program to memory, starting at the pc */
+  return memory.Lc3mem.Writea(Origin, interfacePrgm) /* write the program to memory, starting at the origin */
+}
+
+func Exec() (err error) {
+  (&registers.Lc3registers).Write(registers.R_PC, Origin) /* init pc register */
   return cpu.Run(&memory.Lc3mem, &registers.Lc3registers, &instructions.Lc3instructionSet) /* run the vm */
 }
