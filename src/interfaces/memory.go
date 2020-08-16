cd012723dc4f2d7ee74e30a548fb8270872f29e4
joseph touzet <joseph@pop-os.localdomain>
2020-08-16 21:42:49 +0200

    modified some interfaces

--- src/interfaces/memory.go
+++ src/interfaces/memory.go
@@ -8,4 +8,5 @@ type Memory interface {
   Writea(address interface{}, values interface{}) (n int, err error) /* write an array of values to memory */
   Read(address interface{}) (value interface{}, err error) /* read a single value to memory */
   Reada(address interface{}, values interface{}) (n int, err error) /* write an array of values to memory */
+  Fetch(pc interface{}) (op Op, err error) /* fetch an instruction off of memory */
 }
--- src/interfaces/registers.go
+++ src/interfaces/registers.go
@@ -6,5 +6,5 @@ import (
 type Registers interface {
   Write(reg interface{}, value interface{}) (err error) /* read the value of a register */
   Read(reg interface{}) (value interface{}, err error) /* read the value of a register */
-  Fetch(memory Memory) (op Op, err error) /* fetch an instruction off of memory */
+  Fetch() (value interface{}, err error) /* fetch the pc off of memory */
 }
