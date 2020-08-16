76bb991fc9fa92ff7b8351fb5674f23c1850c817
joseph touzet <joseph@pop-os.localdomain>
2020-08-16 23:59:47 +0200

    moved lc3 instructions constant definition to a separate file

--- src/lc3/instructions/instructions.go
+++ src/lc3/instructions/instructions.go
@@ -5,47 +5,6 @@ import (
   "github.com/jolatechno/go-lc3/src/interfaces"
 )
 
-/* defining the name of all lc3 opcode */
-const (
-    OP_BR = iota     /* branch */
-    OP_ADD = iota    /* add  */
-    OP_LD = iota     /* load */
-    OP_ST = iota     /* store */
-    OP_JSR = iota    /* jump register */
-    OP_AND = iota    /* bitwise and */
-    OP_LDR = iota    /* load register */ /* TODO */
-    OP_STR = iota    /* store register */ /* TODO */
-    OP_RTI = iota    /* unused */ /* TODO */
-    OP_NOT = iota    /* bitwise not */ /* TODO */
-    OP_LDI = iota    /* load indirect */ /* TODO */
-    OP_STI = iota    /* store indirect */ /* TODO */
-    OP_JMP = iota    /* jump */
-    OP_RES = iota    /* reserved (unused) */ /* TODO */
-    OP_LEA = iota    /* load effective address */ /* TODO */
-    OP_TRAP = iota   /* execute trap */
-    OP_COUNT = iota
-)
-
-const (
-	TRAP_GETC  = 0x20 // get character from keyboard
-	TRAP_OUT   = 0x21 // output a character
-	TRAP_PUTS  = 0x22 // output a word string
-	TRAP_IN    = 0x23 // input a string
-	TRAP_PUTSP = 0x24 // output a byte string
-	TRAP_HALT  = 0x25 // halt the program
-)
-
-const (
-	MR_KBSR = 0xFE00 // keyboard status
-	MR_KBDR = 0xFE02 // keyboard data
-)
-
-const (
-	FL_POS = 1 << 0
-	FL_ZRO = 1 << 1
-	FL_NEG = 1 << 2
-)
-
 /* defining a basic interface for lc3 instructions */
 type Lc3Instruction struct {
   OP uint8 /* instruction opcode, uint4 would have been more appropriate but isn't present in standard go */
