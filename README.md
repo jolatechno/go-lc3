035a6cf21bef01244359225b979b2c1e45082298
jolatechno <joseph.touzet@gmail.com>
2020-08-17 18:22:19 +0200

    added info inside of the README file

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
 # go-lc3
 
 The goal of this project is to make a modular golang VM that can at first run LC3, but can then be made to run other instruction set.
+
+## Lc3
+
+The lc3 vm was inspired by [Write your Own Virtual Machine](https://justinmeiners.github.io/lc3-vm/index.html) and [ziggy42/gLC3](https://github.com/ziggy42/gLC3/).
+
+Images are compiled using [lc3web](https://wchargin.github.io/lc3web/).
+
+### Usage
+
+```go
+package main
+
+import (
+  "github.com/jolatechno/go-lc3/src/lc3"
+)
+
+func main() {
+  instructions.DebugLc3Instructions = true /* remove if you don't want debuging */
+
+  _, err := cpu.LoadFile(&lc3.Lc3cpu, "your_image_filename") /* test the loadFile function */
+  if err != nil { /* throwing an error */
+      panic(err)
+  }
+
+  err = cpu.Run(&lc3.Lc3cpu) /* test the run function */
+  if err != nil { /* throwing an error */
+      panic(err)
+  }
+}
+```
--- src/lc3/lc3_test.go
+++ src/lc3/lc3_test.go
@@ -3,17 +3,9 @@ package lc3
 import (
   "fmt"
   "testing"
-  //"encoding/binary"
   "github.com/jolatechno/go-lc3/src/interfaces"
   "github.com/jolatechno/go-lc3/src/cpu"
   "github.com/jolatechno/go-lc3/src/lc3/instructions"
-  //"github.com/jolatechno/go-lc3/src/lc3/registers"
-)
-
-var (
-  //offset uint16 = registers.PC_START
-  //halt uint16 = instructions.OP_TRAP << 12 | instructions.TRAP_HALT
-  //prgm = make([]byte, 4)
 )
 
 func TestLc3Cpu(t *testing.T) {
@@ -23,23 +15,6 @@ func TestLc3Cpu(t *testing.T) {
   if !ok { /* throwing an error */
     t.Error("couldn't convert lc3 cpu to interface")
   }
-
-  /* load halt value in memory */
-  //binary.BigEndian.PutUint16(prgm[0:], offset)
-	//binary.BigEndian.PutUint16(prgm[2:], halt)
-
-  //n, err := (&Lc3cpu).Load(prgm) /* test the load function */
-  //if err != nil { /* throwing an error */
-  //  t.Error(err)
-  //} else if n != len(prgm) - 2 { /* check the written length */
-  //  t.Error("couldn't write the whole program")
-  //} else {
-  //  fmt.Println("") /* jump a line */
-  //  err = cpu.Run(&Lc3cpu) /* test the run function */
-  //  if err != nil { /* throwing an error */
-  //    t.Error(err)
-  //  }
-  //}
 
   _, err := cpu.LoadFile(&Lc3cpu, "hello.obj") /* test the loadFile function */
   if err != nil { /* throwing an error */
