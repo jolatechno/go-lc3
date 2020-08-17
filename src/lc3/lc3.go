89be2b401815204d5fbe70c8503834392e574f78
jolatechno <joseph.touzet@gmail.com>
2020-08-17 18:15:16 +0200

    finished the lc3 vm

--- src/lc3/lc3.go
+++ src/lc3/lc3.go
@@ -1,7 +1,7 @@
 package lc3
 
 import (
-  "errors"
+  "encoding/binary"
   "github.com/jolatechno/go-lc3/src/interfaces"
   "github.com/jolatechno/go-lc3/src/lc3/registers"
   "github.com/jolatechno/go-lc3/src/lc3/memory"
@@ -13,12 +13,6 @@ type Lc3Cpu struct {
   Mem memory.Lc3Mem
   Instrs instructions.Lc3InstructionSet
   Regs registers.Lc3Registers
-}
-
-/* function to convert two bytes to uint16 */
-func byte2uint16(b1 byte, b2 byte) uint16 {
-    _ = b1 // bounds check hint to compiler; see golang.org/issue/14808
-    return uint16(b1) | uint16(b2) << 8
 }
 
 /* and define the variable */
@@ -38,15 +32,14 @@ func (cpu *Lc3Cpu)Registers() (memory interfaces.Registers) {
 }
 
 func (cpu *Lc3Cpu)Load(img []byte) (n int, err error) {
-  if len(img) % 2 == 1 { /* check if the array is of even length */
-    return 0, errors.New("can't convert byte array of uneven length to uint16 array")
+  origin := binary.BigEndian.Uint16(img[0:2]) /* read origin off of the image */
+
+  prgm := make([]uint16, int(len(img) / 2 - 1)) /* create an empty array */
+  for i, j := 2, 0; i < len(img); i += 2 {
+    prgm[j] = binary.BigEndian.Uint16(img[i:i + 2]) /* convert two byte to uint */
+    j++
   }
 
-  prgm := make([]uint16, int(len(img) / 2)) /* create an empty array */
-  for i, _ := range prgm { /* iterate over the array */
-    prgm[i] = byte2uint16(img[2*i], img[2*i + 1]) /* convert two byte to uint */
-  }
-
-  n, err = cpu.Memory().Writea(uint16(0), prgm) /* write program to memory */
+  n, err = cpu.Memory().Writea(origin, prgm) /* write program to memory */
   return n*2, err /* return length in byte and error */
 }
--- src/lc3/lc3_test.go
+++ src/lc3/lc3_test.go
@@ -1,36 +1,53 @@
 package lc3
 
 import (
+  "fmt"
   "testing"
+  //"encoding/binary"
   "github.com/jolatechno/go-lc3/src/interfaces"
   "github.com/jolatechno/go-lc3/src/cpu"
   "github.com/jolatechno/go-lc3/src/lc3/instructions"
-  "github.com/jolatechno/go-lc3/src/lc3/registers"
+  //"github.com/jolatechno/go-lc3/src/lc3/registers"
 )
 
 var (
-  offset uint16 = registers.PC_START
-  halt uint16 = instructions.OP_TRAP << 12 | instructions.TRAP_HALT
-  prgm = make([]byte, 2*registers.PC_START + 2)
+  //offset uint16 = registers.PC_START
+  //halt uint16 = instructions.OP_TRAP << 12 | instructions.TRAP_HALT
+  //prgm = make([]byte, 4)
 )
 
 func TestLc3Cpu(t *testing.T) {
+  instructions.DebugLc3Instructions = true
+
   _, ok := interface{}(&Lc3cpu).(interfaces.Cpu) /* testing if Lc3 implements the right interface */
   if !ok { /* throwing an error */
     t.Error("couldn't convert lc3 cpu to interface")
   }
 
   /* load halt value in memory */
-  prgm[2*registers.PC_START] = byte(halt & 0xFF)
-  prgm[2*registers.PC_START + 1] = byte(halt >> 8)
+  //binary.BigEndian.PutUint16(prgm[0:], offset)
+	//binary.BigEndian.PutUint16(prgm[2:], halt)
 
-  n, err := (&Lc3cpu).Load(prgm) /* test the load function */
+  //n, err := (&Lc3cpu).Load(prgm) /* test the load function */
+  //if err != nil { /* throwing an error */
+  //  t.Error(err)
+  //} else if n != len(prgm) - 2 { /* check the written length */
+  //  t.Error("couldn't write the whole program")
+  //} else {
+  //  fmt.Println("") /* jump a line */
+  //  err = cpu.Run(&Lc3cpu) /* test the run function */
+  //  if err != nil { /* throwing an error */
+  //    t.Error(err)
+  //  }
+  //}
+
+  _, err := cpu.LoadFile(&Lc3cpu, "hello.obj") /* test the loadFile function */
   if err != nil { /* throwing an error */
     t.Error(err)
-  } else if n != len(prgm) { /* check the written length */
-    t.Error("couldn't write the whole program")
   } else {
+    fmt.Println("") /* jump a line */
     err = cpu.Run(&Lc3cpu) /* test the run function */
+    fmt.Println("") /* jump a line */
     if err != nil { /* throwing an error */
       t.Error(err)
     }
